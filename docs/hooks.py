5ef68449fe96c590bff8dd6138b5d0aa40b5e0c0
Opeyemi Okuboyejo <135651560+opeyemitechpro@users.noreply.github.com>
2025-08-26 16:20:23 +0100

    Update

--- docs/hooks.py
+++ docs/hooks.py
@@ -1,12 +1,9 @@
-
 from mkdocs.plugins import BasePlugin
 import json
 
-class PopupPlugin(BasePlugin):
-    def on_page_context(self, context, page, config, nav):
-        popup = page.meta.get("popup")
-        if popup:
-            meta_tag = f'<meta name="mkdocs-page-popup" content="{json.dumps(popup)}">'
-            context["extrahead"] = context.get("extrahead", "") + meta_tag
-        return context
-
+def on_page_context(context, page, config, nav):
+    popup = page.meta.get("popup")
+    if popup:
+        meta_tag = f'<meta name="mkdocs-page-popup" content="{json.dumps(popup)}">'
+        context["extrahead"] = context.get("extrahead", "") + meta_tag
+    return context
