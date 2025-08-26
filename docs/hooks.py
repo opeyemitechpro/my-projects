10d99329ef7cc9620e87861a04d475ef41f7e599
Opeyemi Okuboyejo <135651560+opeyemitechpro@users.noreply.github.com>
2025-08-26 16:27:16 +0100

    Update

--- docs/hooks.py
+++
@@ -1,9 +0,0 @@
-from mkdocs.plugins import BasePlugin
-import json
-
-def on_page_context(context, page, config, nav):
-    popup = page.meta.get("popup")
-    if popup:
-        meta_tag = f'<meta name="mkdocs-page-popup" content="{json.dumps(popup)}">'
-        context["extrahead"] = context.get("extrahead", "") + meta_tag
-    return context
