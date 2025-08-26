fb04582e4a1c60364837e6d23b4f8d86ba82ce41
Opeyemi Okuboyejo <135651560+opeyemitechpro@users.noreply.github.com>
2025-08-26 16:13:05 +0100

    Update

--- docs/plugins/popup.py
+++ docs/plugins/popup.py
@@ -6,7 +6,7 @@ class PopupPlugin(BasePlugin):
     def on_page_context(self, context, page, config, nav):
         popup = page.meta.get("popup")
         if popup:
-            # Inject as JSON in meta tag
-            context["extrahead"] = context.get("extrahead", "") + \
-                f'<meta name="mkdocs-page-popup" content="{json.dumps(popup)}">'
+            meta_tag = f'<meta name="mkdocs-page-popup" content="{json.dumps(popup)}">'
+            context["extrahead"] = context.get("extrahead", "") + meta_tag
         return context
+
